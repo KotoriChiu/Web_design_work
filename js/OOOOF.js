68794785865fb335ffd06ff193ab00dc6bdbe57f
KotoriChiu <zxcv88928@gmail.com>
2019-01-02 23:38:09 +0800

    complete

---
+++ js/OOOOF.js
@@ -0,0 +1,42 @@
+$(function () {
+    $(".flip").click(function () {
+        $(".panel").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+    });
+});
+$(function () {
+    $(".flip2").click(function () {
+        $(".panel2").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+    });
+});
+$(function () {
+    $(".flip3").click(function () {
+        $(".panel3").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+    });
+});
+$(function () {
+    $(".flip4").click(function () {
+        $(".panel4").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+    });
+});
+$(function(){
+    $(".panel5").slideToggle("slow");
+    $(".xs1").toggle();
+    $(".xs2").toggle();
+});
+$(function(){
+    // Helper function to Fill and Center the HTML5 Video
+    jQuery('video, object').maximage('maxcover');
+   });
+
+
+$().ready(function() {
+    $('body').tubular('7QSfebF5dRQ','wrapper-video');
+    });
